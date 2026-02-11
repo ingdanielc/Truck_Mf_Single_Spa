497d8860330e59946a32b3bed97d5b9ce157d01b
Daniel Andrés Castro Solis (GTK) <dsolis@gattaca.co>
2026-02-10 20:30:15 -0500

    Update: Nuevos styles con bootstrap

--- src/Truck-root-config.ts
+++ src/Truck-root-config.ts
@@ -1,52 +1,49 @@
-import { registerApplication, start, LifeCycles } from "single-spa";
+import { registerApplication, start, LifeCycles } from 'single-spa';
 //import { setupInactivityTimer } from './assets/js/inactivityTimer';
-import "./assets/style/template-claro.css";
 
 const apiUrl = process.env['API_URL'];
 const basePath = process.env['BASE_PATH'];
 const TIMEOUT = process.env['TIMEOUT'];
 
-if (window.location.pathname === "/") {
-  window.history.pushState({}, '', "/gym/auth");
+if (globalThis.location.pathname === '/') {
+  globalThis.history.pushState({}, '', '/gym/auth');
 }
 
-if (window.location.pathname === "/gym/") {
-  window.history.pushState({}, '', "/gym/hub/home");
+if (globalThis.location.pathname === '/gym/') {
+  globalThis.history.pushState({}, '', '/gym/hub/home');
 }
 
 registerApplication({
-  name: "Truck_Mf_Home",
+  name: 'Truck_Mf_Home',
   app: () =>
     System.import<LifeCycles>(
-      process.env['NAME'] !== "local"
-        ? `${apiUrl}/truck/truck-mf-home/main.js`
-        : `${apiUrl}:4201/main.js`
+      process.env['NAME'] === 'local'
+        ? `${apiUrl}:4201/main.js`
+        : `${apiUrl}/truck/truck-mf-home/main.js`
     ),
   activeWhen: [(location) => location.pathname.startsWith(`${basePath}/hub`)],
 });
 
 registerApplication({
-  name: "Truck_Mf_Auth",
+  name: 'Truck_Mf_Auth',
   app: () =>
     System.import<LifeCycles>(
-      process.env['NAME'] !== "local"
-        ? `${apiUrl}/truck/truck-mf-auth/main.js`
-        : `${apiUrl}:4200/main.js`
+      process.env['NAME'] === 'local'
+        ? `${apiUrl}:4200/main.js`
+        : `${apiUrl}/truck/truck-mf-auth/main.js`
     ),
   activeWhen: [(location) => location.pathname.startsWith(`${basePath}/auth`)],
 });
 
 registerApplication({
-  name: "Truck_Mf_Site",
+  name: 'Truck_Mf_Site',
   app: () =>
     System.import<LifeCycles>(
-      process.env['NAME'] !== "local"
-        ? `${apiUrl}/truck/truck-mf-site/main.js`
-        : `${apiUrl}:4202/main.js`
+      process.env['NAME'] === 'local'
+        ? `${apiUrl}:4202/main.js`
+        : `${apiUrl}/truck/truck-mf-site/main.js`
     ),
-  activeWhen: [
-    (location) => location.pathname.startsWith(`${basePath}/site`),
-  ],
+  activeWhen: [(location) => location.pathname.startsWith(`${basePath}/site`)],
 });
 
 //setupInactivityTimer(TIMEOUT, '/truck/auth');
